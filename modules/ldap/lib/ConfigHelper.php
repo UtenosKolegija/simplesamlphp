3b2be4dbb8135f5ffc93e3c6bb21aee24aab19d0
Mantas Mikulėnas <grawity@gmail.com>
2014-10-23 14:02:32 +0200

    ldap: implement impersonation by administrators
    copy impersonation ACL attributes from authn to authz
    
    store username in 'authenticatedAs'
    
    ldap: do not allow other administrators to be impersonated
    
    ldap: extract impersonate ACLs to a common function
    
    support authz whitelists/blacklists
    
    ldap: add logging to authz ACL checks
    
    ldap: impersonate: update for namespaces
    
    ldap: impersonate: correctly propagate lack of attribute as well
    
    ldap: clarify impersonation log messages
    
    impersonate: more clearly inform about requisite AllowImpersonateAs

--- modules/ldap/lib/ConfigHelper.php
+++ modules/ldap/lib/ConfigHelper.php
@@ -166,6 +166,62 @@ class sspmod_ldap_ConfigHelper
         $this->attributes = $config->getArray('attributes', null);
     }
 
+    /**
+     * UK: Check whether the user is allowed to impersonate the target.
+     */
+    static function may_impersonate($from, $to, $from_attrs, $to_attrs)
+    {
+        $DOMAIN_ADMIN_DN = "CN=Domain Admins,CN=Users,DC=ad,DC=utenos-kolegija,DC=lt";
+
+        $_stage = $to_attrs ? "post" : "pre";
+
+        $_log = function ($msg) use($from, $to, $_stage) {
+            SimpleSAML\Logger::info("Impersonation by '$from' to '$to'; $_stage check: $msg");
+        };
+
+        $allow = false;
+
+        /* allow or deny based on source account */
+
+        if ($from_attrs) {
+            /* check grant rules */
+            if (!$allow && isset($from_attrs["memberOf"]))
+            {
+                $allow = in_array($DOMAIN_ADMIN_DN, $from_attrs["memberOf"], true);
+                if ($allow) $_log("allow (source is domain-admin)");
+            }
+            if (!$allow && isset($from_attrs["ukAllowImpersonateAs"]))
+            {
+                $allow = in_array("all", $from_attrs["ukAllowImpersonateAs"], true);
+                if ($allow) $_log("allow (ACL on source permits all target users)");
+            }
+            if (!$allow && isset($from_attrs["ukAllowImpersonateAs"]))
+            {
+                $allow = in_array($to, $from_attrs["ukAllowImpersonateAs"], true);
+                if ($allow) $_log("allow (ACL on source permits this target user)");
+            }
+            if (!$allow) $_log("deny (ACL on source doesn't permit this user)");
+
+            /* check deny rules */
+            if ($allow && isset($from_attrs["ukDenyImpersonateAs"]))
+            {
+                $allow = !in_array($to, $from_attrs["ukDenyImpersonateAs"], true);
+                if (!$allow) $_log("deny (ACL on source forbids this target user)");
+            }
+        }
+
+        /* deny based on target account */
+
+        if ($to_attrs) {
+            if ($allow && isset($to_attrs["memberOf"]))
+            {
+                $allow = !in_array($DOMAIN_ADMIN_DN, $to_attrs["memberOf"], true);
+                if (!$allow) $_log("deny (target is domain-admin)");
+            }
+        }
+
+        return $allow;
+    }
 
     /**
      * Attempt to log in using the given username and password.
@@ -182,6 +238,14 @@ class sspmod_ldap_ConfigHelper
     {
         assert(is_string($username));
         assert(is_string($password));
+
+        SimpleSAML\Logger::debug("UK: ConfigHelper.login('$username', <password>) called");
+        if (preg_match('!^([^/]+)/([^/]+)$!', $username, $m)) {
+            list (, $username, $targetname) = $m;
+        } else {
+            $targetname = $username;
+        }
+        SimpleSAML\Logger::debug("UK: ConfigHelper.login() has authn '$username', authz '$targetname'");
 
         if (empty($password)) {
             SimpleSAML\Logger::info($this->location.': Login with empty password disallowed.');
@@ -218,6 +282,8 @@ class sspmod_ldap_ConfigHelper
             $dn = $ldap->whoami($this->searchBase, $this->searchAttributes);
         }
 
+        $authn_dn = $dn;
+
         /* Are privs needed to get the attributes? */
         if ($this->privRead) {
             /* Yes, rebind with privs */
@@ -226,7 +292,81 @@ class sspmod_ldap_ConfigHelper
             }
         }
 
-        return $ldap->getAttributes($dn, $this->attributes);
+        $attrs = $ldap->getAttributes($dn, $this->attributes);
+
+        $authn_attrs = $attrs;
+
+        if ($targetname !== $username) {
+            if (!self::may_impersonate($username, $targetname, $attrs, null)) {
+                SimpleSAML\Logger::error($this->location.": User '$username' attempted to impersonate '$targetname'");
+                throw new SimpleSAML_Error_Error('WRONGUSERPASS');
+            }
+
+            SimpleSAML\Logger::debug("UK: ConfigHelper.login() recursing from '$username' to impersonate '$targetname'");
+
+            if (!$this->searchEnable) {
+                $ldapusername = addcslashes($targetname, ',+"\\<>;*');
+                $dn = str_replace('%username%', $ldapusername, $this->dnPattern);
+            } else {
+                if ($this->searchUsername !== NULL) {
+                    if(!$ldap->bind($this->searchUsername, $this->searchPassword)) {
+                        throw new Exception('Error authenticating using search username & password.');
+                    }
+                }
+
+                $dn = $ldap->searchfordn($this->searchBase, $this->searchAttributes, $targetname, TRUE);
+                if ($dn === NULL) {
+                    /* User not found with search. */
+                    SimpleSAML\Logger::info($this->location . ': Unable to find impersonated users DN. username=\'' . $targetname . '\'');
+                    throw new SimpleSAML_Error_Error('WRONGUSERPASS');
+                }
+            }
+
+            SimpleSAML\Logger::notice("User '$username' has successfully impersonated user '$targetname'.");
+
+            /* Are privs needed to get the attributes? */
+            if ($this->privRead) {
+                /* Yes, rebind with privs */
+                if (!$ldap->bind($this->privUsername, $this->privPassword)) {
+                    throw new Exception('Error authenticating using privileged DN & password.');
+                }
+            } else {
+                if (!$ldap->bind($authn_dn, $password, $sasl_args)) {
+                    throw new Exception("Error re-authenticating using impersonator's username & password.");
+                }
+            }
+        }
+
+        SimpleSAML\Logger::debug("UK: ConfigHelper.login() final DN is '$dn'");
+
+        $attrs = $ldap->getAttributes($dn, $this->attributes);
+
+        if ($dn !== $authn_dn) {
+            /* verify that impersonation is allowed */
+
+            if (!self::may_impersonate($username, $targetname, $authn_attrs, $attrs)) {
+                SimpleSAML\Logger::error($this->location.": User '$username' attempted to impersonate '$targetname'");
+                throw new SimpleSAML_Error_Error('WRONGUSERPASS');
+            }
+
+            /* store impersonator's name */
+
+            $attrs["authenticatedAs"] = array($username);
+            $attrs["authenticatedDN"] = array($authn_dn);
+
+            /* copy authorization-related attributes */
+
+            $dup_attrs = ["ukDenyImpersonateTo", "ukAllowImpersonateTo"];
+
+            foreach ($dup_attrs as $k)  {
+                if (isset($authn_attrs[$k])) {
+                    $attrs[$k] = $authn_attrs[$k];
+                } else {
+                    unset($attrs[$k]);
+                }
+            }
+        }
+        return $attrs;
     }
 
 
